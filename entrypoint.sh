6f404ecfe67ea2a2fb0a081a161d046c9c5d725a
yokiworks <itamarperez@gmail.com>
2020-02-02 21:02:33 -0800

    update kubeconfig path

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 KUBECONFIG=${1}
 PR_REF= ${2}
 
-echo ${{KUBECONFIG}} | base64 -d > ./kubeconfig.yaml
+echo ${{KUBECONFIG}} | base64 -d > /kubeconfig.yaml
 echo ">>>> kubeconfig created"
 
 REGEX="[a-zA-Z]+-[0-9]{1,5}"
@@ -14,9 +14,9 @@ else
     exit 0
 fi
 
-if [[ $(kubectl --kubeconfig=./kubeconfig.yaml -n argocd get application $NAMESPACE) ]]; then
+if [[ $(kubectl --kubeconfig=/kubeconfig.yaml -n argocd get application $NAMESPACE) ]]; then
     echo ">>>> Argo Application exist, Deleating now..."
-    kubectl --kubeconfig=./kubeconfig.yaml -n argocd delete application $NAMESPACE
+    kubectl --kubeconfig=/kubeconfig.yaml -n argocd delete application $NAMESPACE
 else
     echo ">>>> Argo Application Does not exist, nothing to clean. OK!"
 fi
