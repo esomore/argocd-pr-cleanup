6571d0e62f3f906cedfe8b458c764306c6b6f0f3
yokiworks <itamarperez@gmail.com>
2020-02-02 17:29:17 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# PRCleanup docker action
+
+This action will delete the argo app associated with the PR being closed.
+
+## Inputs
+
+### `kubeconfig`
+
+**Required** secrets.ALPHA_KUBECTL.
+
+### `kubeconfig`
+
+**Required** secrets.ALPHA_KUBECTL.
+
+### `pr-ref`
+
+**Required** github.event.pull_request.head.ref.
---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -l
+
+KUBECONFIG=${1}
+PR_REF= ${2}
+
+echo ${{KUBECONFIG}} | base64 -d > ./kubeconfig.yaml
+echo ">>>> kubeconfig created"
+
+REGEX="[a-zA-Z]+-[0-9]{1,5}"
+if [[ $PR_REF =~ $REGEX ]]; then
+    export NAMESPACE=$(echo ${BASH_REMATCH[0]} | tr '[:upper:]' '[:lower:]')
+else
+    echo ">>>> $PR_REF is not a feature branch, nothing to cleanup"
+    exit 0
+fi
+
+if [[ $(kubectl --kubeconfig=./kubeconfig.yaml -n argocd get application $NAMESPACE) ]]; then
+    echo ">>>> Argo Application exist, Deleating now..."
+    kubectl --kubeconfig=./kubeconfig.yaml -n argocd delete application $NAMESPACE
+else
+    echo ">>>> Argo Application Does not exist, nothing to clean. OK!"
+fi
